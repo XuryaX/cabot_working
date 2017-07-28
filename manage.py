241a74902c2c4c64a339714e210abfbfd45dcb0d
Shaurya Chaudhuri <shauryadeepc@hotmail.com>
2017-07-28 19:01:10 +0530

    pollicy

--- manage.py
+++ manage.py
@@ -4,15 +4,11 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cabot.settings")
-
-    from django.core.management import execute_from_command_line
-    print(sys.argv)
-    run = True
-    if run:
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cabot.settings")
+	from django.core.management import execute_from_command_line
+	print(sys.argv)
 	execute_from_command_line(sys.argv)
 	migration = ['manage.py', 'makemigrations']
 	execute_from_command_line(migration)
-
 	migrate = ['manage.py', 'migrate']
 	execute_from_command_line(migrate)
